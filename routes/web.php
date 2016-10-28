45e232924c8b35693b778284aca392f25cf77351
Test User <test@test.ch>
2016-10-28 16:49:25 +0200

    gut

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,50 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+* Display ALL Tasks
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+     $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('task', [
+        'tasks' => $tasks
+    ]);
+});
+
+
+/**
+* Add a new Task
+*/
+
+Route::post('/task', function (Request $request){
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+
+});
+
+
+/**
+* Delete an existing Task
+*/
+
+Route::delete('/task/{id}', function ($id){
+    //
 });
