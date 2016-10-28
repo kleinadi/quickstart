d966cbbc17fb731eba0416a43b17100deb44e654
Test User <test@test.ch>
2016-10-28 17:28:55 +0200

    Fertig mir Controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+    * Display ALL Tasks
+    **/
+    public function index() 
+    {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('task', ['tasks' => $tasks]);
+    }
+
+    /**
+    * Add a new Task
+    */
+
+    public function newTask(Request $request)
+    {
+        $validator = Validator::make($request->all(), ['name' => 'required|max:255',]);
+
+        if ($validator->fails()) 
+        {
+            return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -17,38 +17,14 @@ use Illuminate\Http\Request;
 /**
 * Display ALL Tasks
 */
-
-Route::get('/', function () {
-     $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('task', [
-        'tasks' => $tasks
-    ]);
-});
+Route::get('/', 'TaskController@index');
 
 
 /**
 * Add a new Task
 */
 
-Route::post('/task', function (Request $request){
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-
-});
+Route::post('/task', 'TaskController@newTask');
 
 
 /**
